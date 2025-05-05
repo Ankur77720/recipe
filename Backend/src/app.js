e4e7d24ccdd566822059177e6421f1a473810bb6
Ankur Prajapati <91510065+Ankur77720@users.noreply.github.com>
2025-05-05 13:24:19 +0530

    Add initial frontend assets including CSS, HTML, and SVG icon

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import indexRoutes from './routes/index.routes.js';
-
+import path from 'path';
 
 
 const app = express();
@@ -10,10 +10,21 @@ app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true,
 }));
+
+app.use(express.static(path.join(path.resolve(), 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/', indexRoutes)
+
+app.get('*wild',(req,res)=>{
+    res.sendFile(path.join(path.resolve(), 'public', 'index.html'), (err) => {
+        if (err) {
+            console.error("Error sending index.html:", err);
+            res.status(500).send("Internal Server Error");
+        }
+    });
+})
 
 
 
